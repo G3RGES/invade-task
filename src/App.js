26195b6d742dca444a3e5d752c2ce4ddaea43c6f
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-12 02:52:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Navbar from "./components/Navbar";
 import Products from "./components/Products";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar />
       <Products />
     </div>
   );
