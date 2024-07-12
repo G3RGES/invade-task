2ecbea69d186f3e9c3d2ae2a434c72d007eb1e5a
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-13 02:15:44 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,17 @@ import Category from "./Category";
 import Price from "./Price";
 import Brand from "./Brand";
 
+import products from "../data.json";
+
 const Navbar = ({ handleChange }) => {
   return (
     <div className=" text-lg font-medium bg-blue-500">
       <section className="flex flex-col p-4 justify-around">
         <Category handleChange={handleChange} />
         <hr />
-        <Price handleChange={handleChange} />
-        <hr />
         <Brand handleChange={handleChange} />
+        <hr />
+        <Price handleChange={handleChange} />
       </section>
     </div>
   );
