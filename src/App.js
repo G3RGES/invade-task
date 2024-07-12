77d26b2d06c75dd848f73b3e02cbdefe054becd2
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-13 02:04:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,71 @@
+import { useState } from "react";
+
+// components
 import Navbar from "./components/Navbar";
 import Products from "./components/Products";
 
+// data
+import products from "./data.json";
+import Product from "./components/Product";
+
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  //* input filter
+  // const [query, setQuery] = useState("");
+
+  // -------------input filter
+  // const handleInputChange = (e) => {
+  //   setQuery(e.target.value);
+  // };
+
+  // const filteredItems = products.filter((product) =>
+  //   product.name.toLowerCase().indexOf(query.toLowerCase() !== -1)
+  // );
+
+  // -------------button filter
+  // const handleClick = (event) => {
+  //   setSelectedCategory(event.target.value);
+  // };
+
+  // -------------radio filter
+  const handleChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  const filteredData = (products, selected) => {
+    // filtering input items
+    // if (query) {
+    //   filteredProducts = filteredItems;
+    // }
+
+    var filteredProducts = products;
+    // selected filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, price, brand }) =>
+          category === selected || price === selected || brand || selected
+      );
+    }
+
+    return filteredProducts.map(({ name, price, category, brand, id }) => (
+      <Product
+        key={id}
+        brand={brand}
+        name={name}
+        price={price}
+        category={category}
+      />
+    ));
+  };
+
+  const result = filteredData(products, selectedCategory);
+
   return (
-    <div>
-      <Navbar />
-      <Products />
-    </div>
+    <>
+      <Navbar handleChange={handleChange} />
+      <Products result={result} />
+    </>
   );
 }
 
