cc3bde873bab993a889edbf7580235b4df2e7774
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-13 02:04:56 +0300

    Update Brand.js

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -1,56 +1,77 @@
 import React from "react";
 import "../styles/Brand.css";
+import Input from "./Input";
 
-const Brand = () => {
+const Brand = ({ handleChange }) => {
   return (
     <div className="">
       <h1 className="brand-title">Brand</h1>
 
       <div className="xl:flex xl:flex-row gap-3 justify-around lg:flex lg:flex-col">
         <label className="flex gap-1 items-center">
-          <input value="" type="radio" name="test3" />
+          <input onChange={handleChange} type="radio" value="" name="test1" />
           <span className="">All</span>
         </label>
-        <label className="flex gap-1 items-center">
-          <input value="" title="TechCo" type="radio" name="test3" />
-          <span className="">TechCo</span>
-        </label>
-        <label className="flex gap-1 items-center">
-          <input value="" title="HomeBrew" type="radio" name="test3" />
-          <span className="">HomeBrew</span>
-        </label>
-        <label className="flex gap-1 items-center">
-          <input value="" title="ActiveGear" type="radio" name="test3" />
-          <span className="">ActiveGear</span>
-        </label>
-        <label className="flex gap-1 items-center">
-          <input value="" title="UrbanStyle" type="radio" name="test3" />
-          <span className="">UrbanStyle</span>
-        </label>
-        <label className="flex gap-1 items-center">
-          <input value="" title="AudioPro" type="radio" name="test3" />
-          <span className="">AudioPro</span>
-        </label>
-        <label className="flex gap-1 items-center">
-          <input value="" title="CoffeeLuxe" type="radio" name="test3" />
-          <span className="">CoffeeLuxe</span>
-        </label>
-        <label className="flex gap-1 items-center">
-          <input value="" title="OutdoorX" type="radio" name="test3" />
-          <span className="">OutdoorX</span>
-        </label>
-        <label className="flex gap-1 items-center">
-          <input value="" title="SunShades" type="radio" name="test3" />
-          <span className="">SunShades</span>
-        </label>
-        <label className="flex gap-1 items-center">
-          <input value="" title="FitLife" type="radio" name="test3" />
-          <span className="">FitLife</span>
-        </label>
-        <label className="flex gap-1 items-center">
-          <input value="" title="KitchenAid" type="radio" name="test3" />
-          <span className="">KitchenAid</span>
-        </label>
+        <Input
+          handleChange={handleChange}
+          value=""
+          title="TechCo"
+          name="test1"
+        />
+        <Input
+          handleChange={handleChange}
+          value=""
+          title="HomeBrew"
+          name="test1"
+        />
+        <Input
+          handleChange={handleChange}
+          value=""
+          title="ActiveGear"
+          name="test1"
+        />
+        <Input
+          handleChange={handleChange}
+          value=""
+          title="UrbanStyle"
+          name="test1"
+        />
+        <Input
+          handleChange={handleChange}
+          value=""
+          title="AudioPro"
+          name="test1"
+        />
+        <Input
+          handleChange={handleChange}
+          value=""
+          title="CoffeeLuxe"
+          name="test1"
+        />
+        <Input
+          handleChange={handleChange}
+          value=""
+          title="OutdoorX"
+          name="test1"
+        />
+        <Input
+          handleChange={handleChange}
+          value=""
+          title="SunShades"
+          name="test1"
+        />
+        <Input
+          handleChange={handleChange}
+          value=""
+          title="FitLife"
+          name="test1"
+        />
+        <Input
+          handleChange={handleChange}
+          value=""
+          title="KitchenAid"
+          name="test1"
+        />
       </div>
     </div>
   );
