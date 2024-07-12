80193c38f0b72a184af37ac37d534f9dcbc41dea
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-13 02:04:58 +0300

    Update Category.js

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "../styles/Category.css";
+import Input from "./Input";
 
-const Category = () => {
+const Category = ({ handleChange }) => {
   return (
     <div>
       <h1 className="sidebar-title">Category</h1>
@@ -10,35 +11,34 @@ const Category = () => {
        gap-3 md:flex md:flex-col sm:justify-around"
       >
         <label className="flex gap-1 items-center">
-          <input value="" type="radio" name="test" />
+          <input onChange={handleChange} type="radio" value="" name="test" />
           <span className="">All</span>
         </label>
-        <label className="flex gap-1">
-          <input
-            value="electronics"
-            title="Electronics"
-            type="radio"
-            name="test"
-          />
-          <span className="">Electronics</span>
-        </label>
-        <label className="flex gap-1">
-          <input
-            value="appliances"
-            title="Appliances"
-            type="radio"
-            name="test"
-          />
-          <span className="">Appliances</span>
-        </label>
-        <label className="flex gap-1">
-          <input value="sports" title="Sports" type="radio" name="test" />
-          <span className="">Sports</span>
-        </label>
-        <label className="flex gap-1">
-          <input value="fashion" title="Fashion" type="radio" name="test" />
-          <span className="">Fashion</span>
-        </label>
+
+        <Input
+          handleChange={handleChange}
+          value="electronics"
+          title="Electronics"
+          name="test"
+        />
+        <Input
+          handleChange={handleChange}
+          value="appliances"
+          title="Appliances"
+          name="test"
+        />
+        <Input
+          handleChange={handleChange}
+          value="sports"
+          title="Sports"
+          name="test"
+        />
+        <Input
+          handleChange={handleChange}
+          value="fashion"
+          title="Fashion"
+          name="test"
+        />
       </div>
     </div>
   );
